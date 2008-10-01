585edf084a842cd0e03d7ebe6d86cd3b8881dd66
Jake Douglas <jakecdouglas@gmail.com>
2008-10-01 14:55:41 -0700

    blah

--- lib/cisco.rb
+++ lib/cisco.rb
@@ -1,7 +1,22 @@
 class Cisco < Net::Telnet
   
-  def initialize
-    
+  def initialize(host, login = nil, enable = nil)
+    @host = host
+    @login = login
+    @enable = enable
+    @prompt = /[#>] \z/n
+    super("Host" => @host, "Prompt" => @prompt)
+    login if @login
   end
   
+  def login(password = nil)
+    raise unless password || @login
+    waitfor("String" => "Password:")
+    puts password || @login
+  end
+
+  def enable(password)
+    
+  end
+
 end
