c1c2724daa5c504afbb556000c0b302312a419d1
Jake Douglas <jakecdouglas@gmail.com>
2008-10-03 14:24:56 -0700

    worked on enable and debug output

--- lib/cisco.rb
+++ lib/cisco.rb
@@ -1,23 +1,17 @@
 class Cisco < Net::Telnet
   
-  attr_accessor :verbose
-  
   # set our variables, open the connection, and login if a password is provided
-  def initialize(host, login = nil, enable = nil, verbose = false)
-    @verbose = verbose
+  def initialize(host, login = nil, enable = nil, debug = false)
     @host = host
     @login = login
     @enable = enable
+    @debug = debug
     @prompt = /[#>] \z/n
     super("Host" => @host, "Prompt" => @prompt)
     waitfor("String" => "Password:")
     login if @login
   end
   
-  # if @verbose is set, 
-  def verbose_out
-    
-  end
   # set variable to new pw if one is passed in and send it to the device. once the prompt is returned we are 'logged in'
   def login(password = nil)
     @login = password || @login
@@ -29,7 +23,9 @@ class Cisco < Net::Telnet
 
   def enable(password = nil)
     @enable = password || @enable
-    raise ArgumentError.new("No enable password given!") unless @login
+    raise ArgumentError.new("No enable password given!") unless @enable
+    puts "enable"
+    waitfor("String" => "Password:")
     puts @enable
     waitfor(@prompt)
     @enabled = true
@@ -41,6 +37,26 @@ class Cisco < Net::Telnet
     (puts "exit" and @enabled = false) if @enabled
     (puts "exit" and @logged_in = false) if @logged_in
     super
+  end
+
+  # methods to toggle debug output
+  def debug_on
+    @debug = true
+  end
+  
+  def debug_off
+    @debug = false
+  end
+  
+  private
+  # if @debug is true, send passed text to stdout
+  def debug_out(output)
+    $stdout.puts output if @debug
+  end
+  
+  def puts(txt)
+    debug_out(txt)
+    super(txt)
   end
 
 end
