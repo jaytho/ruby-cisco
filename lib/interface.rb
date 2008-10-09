6491ec26530ce978fdf52ace0e1d0b2769f48415
Jake Douglas <jakecdouglas@gmail.com>
2008-10-09 14:34:09 -0700

    started interface parsing

--- lib/interface.rb
+++ lib/interface.rb
@@ -2,24 +2,29 @@ module Cisco
   
   class Interface
     
+    attr_reader :status, :stats, :mac
+    
     # We pass a parent device into the constructor, as well as the text to parse out it's details.
-    def initialize(parent, text)
-      raise CiscoError.new("Parent must be an instance of Cisco::Cisco.") unless parent.kind_of?(Cisco)
+    def initialize(parent, textary)
+      raise CiscoError.new("Parent must be an instance of Cisco::Base.") unless parent.kind_of?(Cisco::Base)
       @parent = parent
-      set_info(text)
+      set_info(textary)
     end
     
     
     
     def refresh_info
       # do something here to pull relevant text from @parent
-      set_info(text)
+      set_info(textary)
     end
     
     private
     
-    def set_info(text)
+    def set_info(textary)
       #parse out our details and set them on ourself
+      textary.delete_at(0)
+      @status = textary[1]
+      @mac = textary[2].slice(/....\.....\...../)
     end
     
   end
