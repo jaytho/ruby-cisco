d3a17d6762d7c198bfcb10cc6358722405b0baad
Jake Douglas <jakecdouglas@gmail.com>
2008-11-11 19:42:07 -0800

    futzing around

--- lib/ssh.rb
+++ lib/ssh.rb
@@ -1,16 +1,22 @@
 require 'rubygems'
 require 'net/ssh'
 require 'timeout'
+require 'thread'
 
 module Cisco
 
   class SSH
 
-    def initialize(host, options = {})
-      @ssh = Net::SSH.start(host, options)
-      @chan = @ssh.open_channel
-      @chan.send_channel_request("shell")
-      @chan.on_data {|ch, data| @buf << data}
+    attr_reader :chan, :ssh, :thread
+
+    def initialize(*args)
+      @buf = ""
+      @thread = Thread.new {
+        @loop = true
+        @ssh = Net::SSH.start(*args)
+        @chan = @ssh.open_channel {|chan| chan.send_channel_request("shell"); chan.on_data {|ch, data| @buf << data; $stdout.puts data} }
+        @ssh.loop { @loop }
+      }
     end
 
     def puts(data)
@@ -19,11 +25,15 @@ module Cisco
 
     # Look at Telnet and IO's expect to see how this is done
     def expect(prompt)
-      if @buf =~ prompt
-        buf = @buf
-        @buf = ""
-        return buf
-      end
+      Timeout::timeout(10) {
+        while true
+          return @buf if @buf =~ prompt
+        end
+      }
+    end
+
+    def close
+      @ssh.close
     end
 
   end # class SSH
