35b70468f02fff1f732780f999be69cace7b680b
Jake Douglas <jakecdouglas@gmail.com>
2008-11-17 10:40:45 -0800

    Added comments

--- lib/ssh.rb
+++ lib/ssh.rb
@@ -5,40 +5,93 @@ require 'thread'
 
 module Cisco
 
-  class SSH < Thread
+  class SSH
 
-    attr_reader :chan, :ssh, :thread, :buf
+	# Just so we can mess with these as we need to for debugging...
+    attr_reader :ssh, :thread, :inbuf, :outbuf
 
     def initialize(*args)
-      @buf = ""
-      @ssh = Net::SSH.start(*args)
-	super
+		@result = @inbuf = @outbuf = ""
+		# Tells our thread to continue running
+		@threadgo = true
+		# Not sure if we need this but just to be safe...
+		@sem = Mutex.new
+		# generic prompt, should work on most devices for now.
+		@prompt = /[#>]\s?\z/n
+		# Start the SSH session inside of a thread. The purpose of this whole
+		# structure is that we can keep the event loop running inside the thread,
+		# while passing new outgoing commands into the @outbuf from the main thread.
+		# This hopefully allows the channel to remain open and work pseudo-interactively.
+		# Currently not working correctly right now, due to me being a retard.
+		@thread = Thread.new {
+			Net::SSH.start(*args) do |ssh|
+				ssh.open_channel do |chan|
+					# Send the shell request
+					chan.send_channel_request("shell") do |ch, success|
+						# Pass all incoming data to our recv_data method
+						ch.on_data {|ch, data| recv_data(data) }
+						# The idea is that this checks the @outbuf on every pass of the event loop
+						# to see if anything new needs to be sent out. I don't think it is happening
+						# correctly right now, though I just did this and haven't tested it much yet.
+						ch.on_process {|chn| @sem.synchronize { chn.send_data(@outbuf) unless @outbuf.empty? } }
+					end
+	  	    	end
+	  	    	# This is _supposed_ to loop forever, even channel is not active.
+				ssh.loop { true }
+			end
+		}
     end
 
+	# All incoming data should be passed to this method through Channel#on_data
+    def recv_data(data)
+    	# append data to buffer...
+		@inbuf << data
+		# For debugging purposes
+		STDOUT.puts data
+		# If the buffer contains our prompt
+		if @inbuf =~ @prompt
+			# assign it to the @result
+			@result = @inbuf
+			# reset the buffer
+			@inbuf = ""
+			# and stop the thread
+			@threadgo = false
+		end
+    end
+
+	# For sending outgoing commands
     def puts(txt)
-	@loop = true
-      @ssh.open_channel do |chan|
-	chan.on_process {|ch| (chan.close && @loop = false) if @buf =~ /[#>]\s?\z/n }
-      	chan.send_channel_request("shell") do |ch, success|
-		ch.on_data {|ch, data| @buf << data; STDOUT.puts data }
-		ch.send_data(txt + "\n")
-	end
-
-      end
-	@ssh.loop { @loop }
+    	# This just updates the output buffer
+		@sem.synchronize { @outbuf = txt + "\n" }
+		# and tells the thread with the session to run
+		run
     end
 
-    # Look at Telnet and IO's expect to see how this is done
+    def run
+    	# For some reason the thread goes to sleep all the time
+    	# while the event loop is running so we need to keep it going?
+    	# I wish I understood threading better. This might be something
+    	# quirky with the event loop but I'm not sure yet.
+		@threadgo = true
+		while @threadgo 
+			@thread.run
+		end
+    end
+
+    # Look at Telnet and IO's expect to see how this is done.
+    # This is a bit wierd but would probably work if everything else did.
     def expect(prompt)
-      Timeout::timeout(10) {
-        while true
-          return @buf if @buf =~ prompt
-        end
-      }
+		Timeout::timeout(10) {
+			while true
+				(return @result && @result = "") if @result =~ prompt
+			end
+		}
     end
 
     def close
-      @ssh.close
+		@ssh.close
+		@thread.run
+		@thread.kill if @thread.alive?
     end
 
   end # class SSH
